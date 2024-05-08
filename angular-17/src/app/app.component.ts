0fed4be45f77f14c396d056d61bbad5c25f7fc0f
Vitor Bermudes <vitor.bermudes@pdcase.com.br>
2024-05-08 20:34:56 -0300

    added primeng, primeicons and primeflex

--- angular-17/src/app/app.component.ts
+++ angular-17/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+import { SharedModule } from './shared/shared.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, SharedModule, DashboardComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'transparencia-villaggio-fe';
+    title = 'transparencia-villaggio-fe';
+
+    constructor(private primengConfig: PrimeNGConfig) { }
+
+    ngOnInit() {
+        this.primengConfig.ripple = true;
+    }
+
 }
---
+++ angular-17/src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+
+}
